b858c8cd7aaae2a73b709d683b32a588d8200fcf
js10081 <js100810081@gmail.com>
2022-05-24 11:14:49 +0900

    Initial Commit

---
+++ starclone/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  starclone
+//
+//  Created by 이주성 on 2022/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
